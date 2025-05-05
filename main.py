6eb681330272268094257328f8c468e7422a4fb3
서상진 (Seo Sangjin) <67358565+srn13542@users.noreply.github.com>
2025-05-05 22:13:58 +0900

    제발좀돼라라

--- main.py
+++ main.py
@@ -1,198 +1,245 @@
 from inoutput import Juice  #inoutput 패키지의 juice를 가져옴..
 from tkinter import *
+from tkinter import messagebox
 
+#변수 선언
+savedMoney:int = 0       #자판기 내에 '결제되어' 저장된 돈, 있는 돈, 거스름돈으로 반환할 수 있는 돈
+inputMoney:int = 0       #사용자가 input한 돈
+cardMode: bool = False   #카드 결제 가능 여부 플래그
+masterMode: bool = False #마스터 모드 관련 플래그
+
+#음료 목록 초기화
 def defaultJuInput (juArr:list) :
-    #list 초기화 function. 
-    juArr.append(Juice.juice("오아시스", 800, 10, 0, True))
-    juArr.append(Juice.juice("오아시스", 800, 10, 0, True))
-    juArr.append(Juice.juice("아쿠아 제로", 2000, 10, 0, True))
-    juArr.append(Juice.juice("레몬워터", 1800, 10, 0, True))
-    juArr.append(Juice.juice("레몬워터", 1800, 10, 0, True))
-    juArr.append(Juice.juice("옥수수 수염차", 1600, 10, 0, True))
-    juArr.append(Juice.juice("옥수수 수염차", 1600, 10, 0, True))
-    juArr.append(Juice.juice("황금보리", 1600, 10, 0, True))
-    juArr.append(Juice.juice("트레비", 1300, 10, 0, True))
-    juArr.append(Juice.juice("트레비", 1300, 10, 0, True))
-    juArr.append(Juice.juice("밀키스", 1100, 10, 0, True))
-    juArr.append(Juice.juice("펩시콜라", 1100, 10, 0, True))
-    juArr.append(Juice.juice("핫식스", 1300, 10, 0, True))
-    juArr.append(Juice.juice("칠성사이다", 1300, 10, 0, True))
-    juArr.append(Juice.juice("델몬트 망고", 1200, 10, 0, True))
-    juArr.append(Juice.juice("델몬트 망고", 1200, 10, 0, True))
-    juArr.append(Juice.juice("립톤", 1200, 10, 0, True))
-    juArr.append(Juice.juice("델몬트 사과에이드", 1100, 10, 0, True))
-    juArr.append(Juice.juice("델몬트 사과에이드", 1100, 10, 0, True))
-    juArr.append(Juice.juice("델몬트 포도에이드", 1100, 10, 0, True))
-    juArr.append(Juice.juice("가나초코", 900, 10, 0, True))
-    juArr.append(Juice.juice("레쓰비 마일드", 900, 10, 0, True))
-    juArr.append(Juice.juice("펩시 제로 콜라", 1100, 10, 0, True))
-    juArr.append(Juice.juice("핫6 제로", 1300, 10, 0, True))
-    juArr.append(Juice.juice("솔의눈", 1200, 10, 0, True))
-    juArr.append(Juice.juice("레쓰비 카페타임 라떼", 1200, 10, 0, True))
-    juArr.append(Juice.juice("게토레이", 1000, 10, 0, True))
-    juArr.append(Juice.juice("게토레이", 1000, 10, 0, True))
-    juArr.append(Juice.juice("코코리치 포도", 1000, 10, 0, True))
-    juArr.append(Juice.juice("잔치집 식혜", 1000, 10, 0, True))
+    juArr.append(Juice.juice("오아시스", 800, 10, 0))
+    juArr.append(Juice.juice("오아시스", 800, 10, 0))
+    juArr.append(Juice.juice("아쿠아 제로", 2000, 10, 0))
+    juArr.append(Juice.juice("레몬워터", 1800, 10, 0))
+    juArr.append(Juice.juice("레몬워터", 1800, 10, 0))
+    juArr.append(Juice.juice("옥수수수염차", 1600, 10, 0))
+    juArr.append(Juice.juice("옥수수수염차", 1600, 10, 0))
+    juArr.append(Juice.juice("황금보리", 1600, 10, 0))
+    juArr.append(Juice.juice("트레비", 1300, 10, 0))
+    juArr.append(Juice.juice("트레비", 1300, 10, 0))
+    juArr.append(Juice.juice("밀키스", 1100, 10, 0))
+    juArr.append(Juice.juice("펩시콜라", 1100, 10, 0))
+    juArr.append(Juice.juice("핫식스", 1300, 10, 0))
+    juArr.append(Juice.juice("칠성사이다", 1300, 10, 0))
+    juArr.append(Juice.juice("델몬트 망고", 1200, 10, 0))
+    juArr.append(Juice.juice("델몬트 망고", 1200, 10, 0))
+    juArr.append(Juice.juice("립톤", 1200, 10, 0))
+    juArr.append(Juice.juice("델몬트 사과", 1100, 10, 0))
+    juArr.append(Juice.juice("델몬트 사과", 1100, 10, 0))
+    juArr.append(Juice.juice("델몬트 포도", 1100, 10, 0))
+    juArr.append(Juice.juice("가나초코", 900, 10, 0))
+    juArr.append(Juice.juice("레쓰비", 900, 10, 0))
+    juArr.append(Juice.juice("펩시 제로", 1100, 10, 0))
+    juArr.append(Juice.juice("핫6 제로", 1300, 10, 0))
+    juArr.append(Juice.juice("솔의눈", 1200, 10, 0))
+    juArr.append(Juice.juice("레쓰비 라떼", 1200, 10, 0))
+    juArr.append(Juice.juice("게토레이", 1000, 10, 0))
+    juArr.append(Juice.juice("게토레이", 1000, 10, 0))
+    juArr.append(Juice.juice("코코리치 포도", 1000, 10, 0))
+    juArr.append(Juice.juice("잔치집 식혜", 1000, 10, 0))
     return juArr
 
+#관리자 모드에 str 반환하기 위한 코드.. 
 def printJuice (juArr:list) :
+    juStr:str = ""
     for i in juArr:
-        print(i.juName)
+        juStr += f"{i.juName} : {i.juCount} 개, 판매된 갯수: {i.juSellCount}\n"
+    return juStr
 
 def runningJapangi() :    #자판기의 mainFunc() 
     isRun:bool = True
     cmdJpg:str = ""    #cmd 자판기.. 나중에는 버튼으로 바꿔야 하나? gui 먼저 연구해봐도 좋을듯 
-    while(isRun) :
-        print("자판기 프로그램 사용 설명서입니다.")  #임시 gui
-        print("--------------------------------------------------------------------------------------------------------")
-        print("1. 지불 방법 선택")
-        print("  - 현금 cash : 동전이나 지폐를 자판기에 투입해 물품을 구매합니다.")
-        print("  - 카드 card : 현금 투입 없이, 판매중인 물품중 최고 금액을 투입합니다. 이후 현금 사용은 불가능합니다.\n")
-        print("2. 지불")
-        print("  - 현금 : 100원을 투입하려면 100을 입력하세요. (100원, 500원, 1000원만 투입 가능)")
-        print("  - 카드 : 입력 없이 자동으로 잔고가 감소합니다.")
-        print("3. 구매")
-        print(" 원하는 상품의 버튼을 눌러주세요(임시:index)\n")
-        print("명령어 목록    -추후 무슨 버튼에 ficus시 출력되도록 수정해야함")
-        print("  - help 도움 : 프로그램 사용 설명서를 보여줍니다.")
-        print("  - list 목록 : 물품의 모든 목록을 보여줍니다.")
-        print("  - buyable 구매가능목록 : 현재 구매 가능한 물품의 목록을 보여줍니다.\n",
-              "                          다음 단계가 정해지지 않은 경우, 명령어를 입력하지 않았을 때에도 본 목록이 보여집니다.")
-        print("  - 100 500 1000 : 해당되는 금액을 자판기에 투입합니다.")
-        print("  - refund 환불 : 투입한 금액을 환불받습니다.")
-        print("  - exit 나가기 : 자판기 프로그램을 종료합니다.")
-        print("--------------------------------------------------------------------------------------------------------")
-        print("\n 계속하시려면 아무 키나 누르세요")
-        print("TODO:: 사용자가 넣은 금액 1000/500/100 단위로 표시되어야 함")
-        print("TODO:: 투입된 금액도 적어두어야 함")    
-        #입력받아서 대충 값에 따라 switch-case문 돌리기..func 분리 
-             # ++ python에는 switch 대신 발전된 match-case와 딕셔너리 사용 예시가 있다고. 
-             # https://okeybox.tistory.com/395
+    print("자판기 프로그램 사용 설명서입니다.")  #임시 cmd
+    print("--------------------------------------------------------------------------------------------------------")
+    print("1. 지불 방법 선택")
+    print("  - 현금 cash : 동전이나 지폐를 자판기에 투입해 물품을 구매합니다.")
+    print("  - 카드 card : 현금 투입 없이, 판매중인 물품중 최고 금액을 투입합니다. 이후 현금 사용은 불가능합니다.\n")
+    print("2. 지불")
+    print("  - 현금 : 100원을 투입하려면 100을 입력하세요. (100원, 500원, 1000원만 투입 가능)")
+    print("  - 카드 : 입력 없이 자동으로 잔고가 감소합니다.")
+    print("3. 구매")
+    print(" 원하는 상품의 버튼을 눌러주세요(임시:index)\n")
+    print("명령어 목록    -추후 무슨 버튼에 ficus시 출력되도록 수정해야함")
+    print("  - help 도움 : 프로그램 사용 설명서를 보여줍니다.")
+    print("  - list 목록 : 물품의 모든 목록을 보여줍니다.")
+    print("  - buyable 구매가능목록 : 현재 구매 가능한 물품의 목록을 보여줍니다.\n",
+          "                          다음 단계가 정해지지 않은 경우, 명령어를 입력하지 않았을 때에도 본 목록이 보여집니다.")
+    print("  - 100 500 1000 : 해당되는 금액을 자판기에 투입합니다.")
+    print("  - refund 환불 : 투입한 금액을 환불받습니다.")
+    print("  - exit 나가기 : 자판기 프로그램을 종료합니다.")
+    print("--------------------------------------------------------------------------------------------------------")
+    print("\n 계속하시려면 아무 키나 누르세요")
+    print("TODO:: 사용자가 넣은 금액 1000/500/100 단위로 표시되어야 함")
+    print("TODO:: 투입된 금액도 적어두어야 함")    
 
+#거스름돈 반환 함수
+def changeMoney() :
+    global inputMoney
+    mon1000 = inputMoney//1000
+    inputMoney -= mon1000*1000
+    mon500 = inputMoney//500
+    inputMoney -= mon500*500
+    mon100 = inputMoney//100
+    inputMoney -= mon100*100
+    mon50 = inputMoney//50
+    inputMoney -= mon50*50
 
-def someBtnEvent() :
-    button['text'] = "버튼 누름"
+    explLabel['text'] = f"거스름돈 1000원 {mon1000}장, 500원 {mon500}개, 100원 {mon100}개, 50원 {mon50}개 반환되었습니다."
+    moneyLabel['text'] = f"{inputMoney}원"
+    canBuyJuice()
+
+def insertMoney(amount: int) :
+    global inputMoney, cardMode
+    inputMoney += amount
+    cardMode = False
+    print(f"현재 투입된 금액: {inputMoney}원")
+    moneyLabel['text'] = f"{inputMoney}원"
+    updateJuiceButtons()
+
+def selectCard() :
+    global cardMode
+    cardMode = True
+    explLabel['text'] = "카드 결제 모드: 모든 음료 구매 가능"
+    updateJuiceButtons()
+
+#구매 가능 여부 판단
+def canBuyJuice() :
+    global inputMoney, cardMode
+    for juice in juArr: 
+        if cardMode :
+            juice.juCanBuy = True
+        else :
+            juice.juCanBuy = inputMoney >= juice.juPrice
+
+#버튼 색상 업데이트
+def updateJuiceButtons():
+    canBuyJuice()
+    for juice, button in juiceWidgets :
+        if juice.juCanBuy :
+            button['bg'] = 'green'
+        else :
+            button['bg'] = 'red'
+
+#음료 구매 시도 
+def attemptPurchase(index):
+    global inputMoney, savedMoney, cardMode
+    juice = juArr[index]
+    if juice.juCanBuy and juice.juCount > 0:
+        juice.juCount -= 1
+        juice.juSellCount += 1
+        if not cardMode:
+            inputMoney -= juice.juPrice
+            savedMoney += juice.juPrice
+        elif cardMode:
+            savedMoney += juice.juPrice
+            cardMode = False
+        explLabel['text'] = f"{juice.juName} 구매 완료!"
+        moneyLabel['text'] = f"{inputMoney}원"
+        updateJuiceButtons()
+    else:
+        explLabel['text'] = "잔액 부족 또는 품절입니다."
+    showSavedMoney['text'] = f"자판기로 번 돈: {savedMoney}원"
+    checkThing()
+
+def checkThing() :
+    global inputMoney, savedMoney, cardMode
+    print(f"inputMoney = {inputMoney}, savedMoney = {savedMoney}, cardMode = {cardMode}")
+
+def showJuiceInfoDialog() :  #참고: https://blog.naver.com/kquddlr/223773510071
+    messagebox.showinfo("주스 갯수와 항목", printJuice(juArr))
+
+def masterShow() :
+    global masterMode
+    if masterMode == True: 
+        masterFrame.pack_forget()
+        masterMode = False
+        explLabel['text'] = "관리자 모드 종료"
+    else : 
+        masterFrame.pack(fill = "both", side = TOP, padx = 5, pady = 5)
+        masterMode = True
+        explLabel['text'] = "관리자 모드 진입"
 
 if __name__ == "__main__":
-    #변수 선언
-    savedMoney:int = 0     #자판기 내에 '결제되어' 저장된 돈, 있는 돈, 거스름돈으로 반환할 수 있는 돈
-    inputMoney:int = 0     #사용자가 input한 돈
-    input1000 :int = 0     #사용자가 1000원 몇 개 넣었는지
-    input500  :int = 0     #사용자가 500원 몇 개 넣었는지
-    input100  :int = 0     #사용자가 100원 몇 개 넣었는지지
 
-    print ("자판기 프로그램 사용 설명서입니다.")
-
-    juArr = []             #주스의 정보를 담을 배열, 
-    defaultJuInput(juArr)  #주스 list 초기화
-    printJuice(juArr)      #테스트용, 주스list 출력, 정상적 출력 확인(추후 삭제)
-
-    #돈을 인풋받기 위한..
-
-    #그 전에 tkinter gui 화면부터 먼저 만들기로 함
     #루트 화면(root Wondow) 생성/설정
     tk = Tk()
     tk.title("자판기")
-    tk.geometry("600x800+500+300")
+    tk.geometry("800x1000+500+300")
     tk.resizable(False,False)
-    
-    #frame1을 임시.. 버튼 넣는 곳으로 지정. 
-    mainFrame = Frame(tk, relief="solid", bd=10, width="600", height="10")
-    #텍스트 표시
-    label = Label(mainFrame, text="자판기 프로그램 임시 gui \n")
-    label.pack() #label 배치 실행
-    # btnFrame1 = Frame(mainFrame, width="600")  
 
-    #버튼들.. 임시로
-    # button1 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button2 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button3 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button4 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button5 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button6 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button7 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button8 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button9 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button10 = Button(btnFrame1, text="Button-1\n", height=10)
-    # button1.pack(side="left", fill="y", expand="true")
-    # button2.pack(side="left", fill="y", expand="true")
-    # button3.pack(side="left", fill="y", expand="true")
-    # button4.pack(side="left", fill="y", expand="true")
-    # button5.pack(side="left", fill="y", expand="true")
-    # button6.pack(side="left", fill="y", expand="true")
-    # button7.pack(side="left", fill="y", expand="true")
-    # button8.pack(side="left", fill="y", expand="true")  
-    # button9.pack(side="left", fill="y", expand="true")
-    # button10.pack(side="left", fill="y", expand="true")  
+    runningJapangi()
+    juArr = []             #주스의 정보를 담을 배열, 
+    defaultJuInput(juArr)  #주스 list 초기화
 
-    # btnFrame2 = Frame(mainFrame, width="600")  
-
-    # button11 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button12 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button13 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button14 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button15 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button16 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button17 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button18 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button19 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button20 = Button(btnFrame2, text="Button-1\n", height=10)
-    # button11.pack(side="left", fill="y", expand="true")
-    # button12.pack(side="left", fill="y", expand="true")
-    # button13.pack(side="left", fill="y", expand="true")
-    # button14.pack(side="left", fill="y", expand="true")
-    # button15.pack(side="left", fill="y", expand="true")
-    # button16.pack(side="left", fill="y", expand="true")
-    # button17.pack(side="left", fill="y", expand="true")
-    # button18.pack(side="left", fill="y", expand="true")  
-    # button19.pack(side="left", fill="y", expand="true")
-    # button20.pack(side="left", fill="y", expand="true") 
-
-    # btnFrame3 = Frame(mainFrame, width="600")  
-
-    # button21 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button22 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button23 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button24 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button25 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button26 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button27 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button28 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button29 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button30 = Button(btnFrame3, text="Button-1\n", height=10)
-    # button21.pack(side="left", fill="y", expand="true")
-    # button22.pack(side="left", fill="y", expand="true")
-    # button23.pack(side="left", fill="y", expand="true")
-    # button24.pack(side="left", fill="y", expand="true")
-    # button25.pack(side="left", fill="y", expand="true")
-    # button26.pack(side="left", fill="y", expand="true")
-    # button27.pack(side="left", fill="y", expand="true")
-    # button28.pack(side="left", fill="y", expand="true")  
-    # button29.pack(side="left", fill="y", expand="true")
-    # button30.pack(side="left", fill="y", expand="true") 
-
-    # btnFrame1.pack(fill="x", expand=True)
-    # btnFrame2.pack(fill="x", expand=True)
-    # btnFrame3.pack(fill="x", expand=True)
+    mainFrame = Frame(tk, relief="solid", bd=10, width="800", height="10", padx = 5, pady = 5)
 
     juiceFrame:Frame = []
+    juButtonList = []
+    juiceWidgets = []  #[(juice_obj, button_obj 식으로 저장할 수 있도록 할 예정.)]
+
     for i in range(0,3):
-        juFrame =  Frame(mainFrame, width="600")
+        juFrame =  Frame(mainFrame, width = 800, padx = 10, pady = 10)
         for j in range(0, 10):
-            juButton = Button(juFrame, text=j, height=10)
-            juButton.pack(side="left", fill="both", expand="true")
-        juFrame.pack(fill="x", expand="true")
-    mainFrame.pack(fill="both")
+            index = (i * 10) + j
+            juice = juArr[index]
+
+            juFrame2 = Frame(juFrame, relief = SOLID, bd = 2, height = 10, width = 5)
+            juButton = Button(juFrame2, text = juice.juPrice, command = lambda idx = index: attemptPurchase(idx))
+            juImg = Label(juFrame2, text = juArr[(i*10)+(j+1)-1].juName, height = 8, width = 5)
+            
+            juiceWidgets.append((juice, juButton))
+
+            juImg.pack(side = TOP, fill = BOTH, expand = TRUE)
+            juButton.pack(side = TOP, fill = BOTH, expand = TRUE)
+            juFrame2.pack(side = LEFT, fill = BOTH, expand = TRUE)
+        juFrame.pack(fill = "x", expand = "true")
+    mainFrame.pack(fill = "both")
+
+    payFrame = Frame(tk)
+    btn1000 = Button(payFrame, text = "1000원", width = 10, command = lambda: insertMoney(1000))
+    btn500  = Button(payFrame, text = "500원", width = 10,  command = lambda: insertMoney(500))
+    btn100  = Button(payFrame, text = "100원", width = 10,  command = lambda: insertMoney(100))
+    btn50   = Button(payFrame, text = "50원" , width = 10,  command = lambda: insertMoney(50))  #인자가 필요하기에 lambda를 쓴다. 
+    cashBtn = Button(payFrame, text="현금", width = 20)
+    cardBtn = Button(payFrame, text="카드", width = 20, command = selectCard)
+    refundBtn = Button(payFrame, text = "거스름돈 반출", width = 20, command = changeMoney)  #혹은 command = changeMoney의 식으로, 함수 참조만 전달할 수 있다고 한다. 
+    moneyLabel = Label(payFrame, text = "0원", width = 20, bg = 'white', bd = 1, relief = SOLID)
+    explLabel = Label(payFrame, text = "설명이 출력되는 창입니다.", bg = 'white', bd = 1, relief = SOLID, width = 10, height = 5)
+    managerBtn = Button(payFrame, text = "관리자 모드 전환 버튼", width = 20, command = masterShow)
+
+    btn1000.grid(row = 0, column = 0, padx = 5, pady = 5)
+    btn500.grid(row = 0, column = 1, padx = 5, pady = 5)
+    btn100.grid(row = 0, column = 2, padx = 5, pady = 5)
+    btn50.grid(row = 0, column = 3, padx = 5, pady = 5)
+    cashBtn.grid(row = 0, column = 4, padx = 10) #side로 배치설정, padx로 좌우여백 설정, pady로 상하여백설정
+    cardBtn.grid(row = 1, column = 4, padx = 10)  #text 수정 희망 시 button['text']에 접근하여 수정인가봄
+    moneyLabel.grid(row = 1, column = 0, columnspan = 4, padx = 5, pady = 5)
+    refundBtn.grid(row = 2, column = 4, padx = 10, pady = 5)
+    explLabel.grid(row=5, column=0, columnspan=5, rowspan = 3, padx=5, pady=10, sticky='we')
+    managerBtn.grid(row = 8, column = 4, padx = 5, pady = 90, sticky = 'se')
+    payFrame.pack(fill = BOTH, pady = 30, padx = 10, side = RIGHT)
+
+    updateJuiceButtons()
 
 
-    frame3 = Frame(tk, width="600")
-    frame4 = Frame(tk, width="600", height="10")
-
-
-    button = Button(tk, text="버튼입니다. 누르면 함수가 실행됩니다.", command = someBtnEvent)
-    button2 = Button(tk, text="버튼2입니다.")
-    button.pack(side=LEFT, padx = 10, pady = 10) #side로 배치설정, padx로 좌우여백 설정, pady로 상하여백설정
-    button2.pack(side=LEFT, padx = 10, pady = 10)  #text 수정 희망 시 button['text']에 접근하여 수정인가봄
+    #--------------------Master Mode
+    masterFrame = Frame(tk, relief = SOLID, bd = 2, width = 800, height = 500)
+    #자판기 관리자 모드에서, 이미 있는 배열을 넘어서도록 음료수를 추가할 수는 없게 할 예정
+    #단, 이미 있는 음료수들의 '종류'와 '갯수'를 수정하고, 
+    #음료수가 판매된 갯수, 등을... 알 수 있도록 한다...
+    # showJuiceCountxt = printJuice(juArr)
+    showSavedMoney = Label(masterFrame, text = f"자판기로 번 돈: {savedMoney}원")
+    inputJuice  = Button(masterFrame, text = "음료수 갯수 수정")
+    editJuice   = Button(masterFrame, text = "음료수 재고 충전")
+    showJpgInfo = Button(masterFrame, text = "자판기 내 음료 정보 확인", command = lambda : showJuiceInfoDialog())
+    showSavedMoney.pack(side = TOP, fill = X, padx = 5, pady = 5)
+    inputJuice.pack(side = TOP, fill = X, padx = 5, pady = 5)
+    editJuice.pack(side = TOP, fill = X, padx = 5, pady = 5)
+    showJpgInfo.pack(side = TOP, fill = X, padx = 5, pady = 5)
+    # masterFrame.pack(fill = "both", side = TOP)
     tk.mainloop()
 
 
